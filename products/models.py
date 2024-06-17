27dab290fb2e002996ad82f0345046d2015e144c
Jeremy Amoako <jeremyamoako1998@gmail.com>
2024-06-17 10:28:46 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=254)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
     def __str__(self):
         return self.name
     
