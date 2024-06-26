bc36f30502b942e44dd16fd76a04a94b70a241e7
Jeremy Amoako <jeremyamoako1998@gmail.com>
2024-06-26 00:29:59 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PJPd8RsgaRubizUyAxIAx5z9izekVbLaXWyfQOLTcixK9081yPasSmMPp3i5EEkhEVIE4QOUCFH1jHj5nLElMlr00Q89od5Ej',
+        'client_secret' : 'test client secret',
     }
 
     return render(request, template, context)
