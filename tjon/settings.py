60d72f297193941e89b6684decb0d23cd9c4529e
Jeremy Amoako <jeremyamoako1998@gmail.com>
2024-07-04 11:05:26 +0000

    updated settings.py

--- tjon/settings.py
+++ tjon/settings.py
@@ -223,7 +223,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+    EMAIL_PORT = 587
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
