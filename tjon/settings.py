3566e6c2c19cc4fa58b81cb17dadd16b7632e142
Jeremy Amoako <jeremyamoako1998@gmail.com>
2024-07-04 11:14:45 +0000

    updated settings.py

--- tjon/settings.py
+++ tjon/settings.py
@@ -225,7 +225,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.egetenv('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
